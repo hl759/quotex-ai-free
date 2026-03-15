d3d6d01304f52184ec307fb7806a254c133fdc17
hl759 <hls4ntiago@gmail.com>
2026-03-15 11:55:12 -0300

    Refactor update_stats method for asset tracking

--- learning_engine.py
+++ learning_engine.py
@@ -1,10 +1,18 @@
 class LearningEngine:
+
     def __init__(self):
         self.asset_stats = {}
 
     def update_stats(self, signals):
-        for signal in signals:
-            asset = signal["asset"]
+
+        for s in signals:
+
+            asset = s["asset"]
+
             if asset not in self.asset_stats:
-                self.asset_stats[asset] = {"signals": 0}
+
+                self.asset_stats[asset] = {
+                    "signals": 0
+                }
+
             self.asset_stats[asset]["signals"] += 1
