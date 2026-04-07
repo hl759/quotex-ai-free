03a7140a1383591f90c44d3df97a8e998eaedf4c
hl759 <hls4ntiago@gmail.com>
2026-04-07 20:08:49 -0300

    Add files via upload

---
+++ learning_repo.py
@@ -0,0 +1,6 @@
+from __future__ import annotations
+
+from alpha_hive.learning.learning_engine import LearningEngine
+
+class LearningRepository(LearningEngine):
+    pass
---
+++ specialists_repo.py
@@ -0,0 +1,6 @@
+from __future__ import annotations
+
+from alpha_hive.learning.specialist_reputation_engine import SpecialistReputationEngine
+
+class SpecialistsRepository(SpecialistReputationEngine):
+    pass
