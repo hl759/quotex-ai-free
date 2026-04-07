23d3483fd36e9c7ba7a3951731a2ca025a29386b
hl759 <hls4ntiago@gmail.com>
2026-04-07 20:02:49 -0300

    Delete test_learning_engine.py

--- test_learning_engine.py
+++
@@ -1,7 +0,0 @@
-from alpha_hive.learning.learning_engine import LearningEngine
-
-def test_learning_engine_updates_segments():
-    engine = LearningEngine()
-    engine.register_outcome("BTCUSDT", "CALL", "trend", "trend", "binance", "crypto", "10:00", "favoravel", "WIN")
-    adj = engine.segment_adjustment("BTCUSDT", "CALL", "trend", "trend", "binance", "crypto", "10:00", "favoravel")
-    assert "proof_state" in adj
